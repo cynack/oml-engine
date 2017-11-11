a412ebd2968f6bf5182f1e5277cd915e54221b28
Hirano Takanori <hrn.takanori@gmail.com>
2017-11-11 23:26:28 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,89 @@
+/*global window*/
+import OMLElement from './OMLElement'
+import * as THREE from 'three'
+
+class OML {
+  constructor(container, sOML) {
+    this.skyboxSize = 100
+
+    this.renderer = new THREE.WebGLRenderer({antialias: true})
+    this.renderer.setSize(container.clientWidth, container.clientHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.vr.enabled = true
+    this.renderer.shadowMap.enabled = true
+    container.appendChild(this.renderer.domElement)
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(75, container.clientWidth/container.clientHeight, 0.1, 1000)
+    this.cameraCtl = new THREE.Group()
+    this.cameraCtl.quaternion.set(0, 0, 0, 1)
+    this.cameraCtl.add(this.camera)
+    this.scene.add(this.cameraCtl)
+
+    this.light = new THREE.DirectionalLight(0xffffff, 0.7)
+    this.light.castShadow = true
+    this.light.shadow.camera.near = 0
+    this.light.shadow.camera.far = 128
+    this.light.shadow.camera.top = 64
+    this.light.shadow.camera.bottom = -64
+    this.light.shadow.camera.right = 64
+    this.light.shadow.camera.left = -64
+    this.light.shadow.mapSize.width = 8192
+    this.light.shadow.mapSize.height = 8192
+    this.scene.add(this.light)
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+    this.scene.add(this.ambientLight)
+    this._setLight([60, 30, 0])
+
+    const cubeLoader = new THREE.CubeTextureLoader()
+    cubeLoader.load([
+      'img/skybox_east.jpg',
+      'img/skybox_west.jpg',
+      'img/skybox_up.jpg',
+      'img/skybox_down.jpg',
+      'img/skybox_north.jpg',
+      'img/skybox_south.jpg'
+    ], (cubeTexture)=>{
+      const cubeShader = THREE.ShaderLib[ 'cube' ]
+      cubeShader.uniforms[ 'tCube' ].value = cubeTexture
+      cubeShader.uniforms[ 'tFlip' ].value = 1
+      const skyBoxMaterial = new THREE.ShaderMaterial({
+        fragmentShader: cubeShader.fragmentShader,
+        vertexShader: cubeShader.vertexShader,
+        uniforms: cubeShader.uniforms,
+        depthWrite: false,
+        side: THREE.BackSide
+      })
+      const skyMesh = new THREE.Mesh(new THREE.BoxGeometry(this.skyboxSize, this.skyboxSize, this.skyboxSize, 1, 1, 1), skyBoxMaterial)
+      this.scene.add(skyMesh)
+    })
+    this.renderer.animate(()=>{this._animate.call(this)})
+    if(sOML)this.viewOML(sOML)
+  }
+
+
+  viewOML(sOML) {
+    throw new Error('Not implemented exception.')
+  }
+
+
+  _setLight(params) {
+    var lightVector = new THREE.Vector3(0, 0, 1)
+    if(params.length == 3) {
+      var eu = new THREE.Euler(-Math.PI*params[0]/180, -Math.PI*params[1]/180, Math.PI*params[2]/180, 'XYZ')
+      lightVector.applyEuler(eu)
+    }
+    if(params.length==4) {
+      var qt = new THREE.Quaternion(params[0], params[1], -params[2], Math.PI*params[3]/180)
+      lightVector.applyQuaternion(qt)
+    }
+    this.light.position.set(lightVector.x, lightVector.y, lightVector.z)
+    this.ambientLight.position.set(lightVector.x, lightVector.y, lightVector.z)
+  }
+
+
+  _animate() {
+    this.renderer.render(this.scene, this.camera)
+  }
+}
+
+window.OML = OML
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'main.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'oml.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env']
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
