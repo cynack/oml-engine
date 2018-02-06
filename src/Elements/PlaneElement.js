d4ad7be50d073028b937a840bc10deb97e2b8e79
Takanori Hirano <hrn.takanori@gmail.com>
2018-02-06 15:51:20 +0900

    fix PlaneElement

--- src/Elements/PlaneElement.js
+++ src/Elements/PlaneElement.js
@@ -4,10 +4,13 @@ import * as THREE from 'three'
 export default class SphereElement extends OMLElement {
   constructor(OML, property) {
     super(OML, property, 2)
-    const obj = new THREE.Mesh(
+    const obj = new THREE.Group()
+    const cube = new THREE.Mesh(
       new THREE.PlaneGeometry(OML._size[0], OML._size[1]),
       new THREE.MeshPhongMaterial({color: OML._color})
     )
+    cube.rotation.set(-Math.PI/2, 0, 0)
+    obj.add(cube)
     super._addObj(obj)
   }
 }
